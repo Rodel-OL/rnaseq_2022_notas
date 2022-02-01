940c758fda8d362ba6b3b6182ee46092724e693d
Rodelmar Ocampo Luna <joserodelmar@gmail.com>
2022-02-01 13:56:58 -0600

    notas de resumenes de bioconductor

---
+++ R/03-notas-biocon.R
@@ -0,0 +1,70 @@
+# >>rmspc
+# Descripcion:
+#   Multiple Sample Peak Calling,
+#   Analisis de resultados de
+#   ChIP-seq
+# Observaciones:
+#   No funciona en todas las
+#   plataformas.
+#   Tiene pocas dependencias
+# link:
+#   http://bioconductor.org/packages/rmspc/
+#
+# >>omixer
+# Descripcion:
+#   Aleatorizacion de muestras
+#   y su distribucion
+# Observaciones:
+#   Ya tiene 18 meses en
+#   bioconductor, y la unica
+#   duda que hay no ha sido
+#   contestada, pero funciona
+#   en todas las plataformas
+# link:
+#   http://bioconductor.org/packages/Omixer/
+## Paquete: microbiomeMarker
+## Resumen: Facilita la identificacion de marcadores microbianos.
+## Plataformas: todas
+## Respuesta: en ocasiones
+
+## Paquete: MicrobiomeProfiler
+## Resumen: Ayuda al analisis de datos bacterianos.
+## Plataformas: todas
+## Respuesta: no
+
+# CellBarcode
+#
+# Resumen: Análisis de códigos de barras de DNA.
+#
+# Utilidades: Puede ser usado en estudios de rastreo de linajes y seguimiento de clones.
+#
+# https://bioconductor.org/packages/CellBarcode/
+#
+#
+#     Methylclock - DNA methylation-based clocks
+#
+# resumen: Estimar edad del reloj biológico y de metilación del DNA.
+#
+# Utilidades: Podría usarse para calcular la edad del dna mitocondrial para análisis posteriores
+#
+# https://bioconductor.org/packages/methylclock/
+#
+# spatzie
+#
+# Descripción: identificación de motivos co-enriquecidos de interacciones enhancer-promotor
+#
+# Utilidades: Definir grupos de interacciones y buscar pares de motivos enriquecidos.
+#
+# Funciona en todas las plataformas.
+#
+# http://bioconductor.org/packages/release/bioc/html/spatzie.html
+#
+# Paquete: monaLisa
+
+# Descripción: Trabaja con motivos en el análisis de datos genómicos
+
+# Utilidades: Anotación de secuencias o  regiones genómicas con motivos que originan cambios en la expresión
+
+# Funciona en todas las plataformas
+#
+# http://bioconductor.org/packages/release/bioc/html/monaLisa.html
